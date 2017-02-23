cac217e2965195348de1c119562480f3801d833e
zhu1387 <zhu.1387@osu.edu>
2017-02-22 23:56:05 -0500

    Update Quote.cs

--- Models/Quote.cs
+++ Models/Quote.cs
@@ -4,7 +4,7 @@ namespace FisherInsuranceApi.Models
     public class Quote
     {
     public int Id { get; set; }
-    public string Product { get; set; }
+    public string PolicyNumber { get; set; }
     public DateTime ExpireDate { get; set; }
     public decimal Price { get; set; }
     }
