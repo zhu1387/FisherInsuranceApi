782383433cbc9b0815257c517c60ca94621f7e5f
Damon <Damon>
2017-04-03 18:32:17 -0400

    done lab08 partial

--- Security/JwtProvider.cs
+++ Security/JwtProvider.cs
@@ -9,6 +9,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace FisherInsuranceApi.Security
 {
@@ -84,15 +85,25 @@ namespace FisherInsuranceApi.Security
                     DateTime now = DateTime.UtcNow;
 
                     //create the claims about the user for the token
-                    var claims = new[]
+                    var claims = new List<Claim>()
                     {
                         new Claim(JwtRegisteredClaimNames.Iss, Issuer),
                         new Claim(JwtRegisteredClaimNames.Sub, user.Id),
                         new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                         new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(now)
                                                                     .ToUnixTimeSeconds()
-                                                                    .ToString(), ClaimValueTypes.Integer64)
+                                                                    .ToString(), ClaimValueTypes.Integer64),
+                        new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                        new Claim(JwtRegisteredClaimNames.GivenName, user.UserName)
                     };
+
+                    var roles = await UserManager.GetRolesAsync(user);
+
+                    foreach (var role in roles)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, role));
+                    }
+
 
                     //create the actual token
                     var token = new JwtSecurityToken(
